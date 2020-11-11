c8353049b8def64c1e1712f877ee3d0971d75c99
Burak Saraloglu <buraksaraloglu1@gmail.com>
2020-11-11 23:20:32 +0300

    localStorage auth fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 // import { Header } from './components/layout/Header';
 // import { Content } from './components/layout/Content';
 // import { BoardsProvider, SelectedBoardProvider } from './context';
@@ -10,24 +10,25 @@ import { useAuthListener } from './hooks';
 
 export const App = () =>
 {
-  const user = useAuthListener();
+  const user = useAuthListener().user;
 
   return (
     <Router>
-      <IsUserRedirect user={user} loggedInPath={ROUTES.DASHBOARD} path={ROUTES.SIGN_IN} exact>
-        <Signin />
-      </IsUserRedirect>
-      <IsUserRedirect user={user} loggedInPath={ROUTES.DASHBOARD} path={ROUTES.SIGN_UP} exact>
-        <Signup />
-      </IsUserRedirect>
-      <IsUserRedirect user={user} loggedInPath={ROUTES.DASHBOARD} path={ROUTES.HOME} exact>
-        <Home />
-      </IsUserRedirect>
+      <Switch>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.DASHBOARD} path={ROUTES.SIGN_IN} exact>
+          <Signin />
+        </IsUserRedirect>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.DASHBOARD} path={ROUTES.SIGN_UP} exact>
+          <Signup />
+        </IsUserRedirect>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.DASHBOARD} path={ROUTES.HOME} exact>
+          <Home />
+        </IsUserRedirect>
 
-      <ProtectedRoute user={user} path={ROUTES.DASHBOARD} exact>
-        <Dashboard />
-      </ProtectedRoute>
-
+        <ProtectedRoute user={user} path={ROUTES.DASHBOARD} exact>
+          <Dashboard />
+        </ProtectedRoute>
+      </Switch>
     </Router>
 
     // <SelectedBoardProvider>
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 export default function Dashboard() {
+
+  // Boards container
   return (
     <>
       <p>hello dashboard</p>
