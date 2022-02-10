0a6bfec0972250b4d7cf43a44c3106cdae74dddc
Burak Saraloglu <buraksaraloglu1@gmail.com>
2022-02-10 21:55:36 +0300

    project cleanup

--- src/pages/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Home } from './home';
-export { default as Dashboard } from './dashboard';
-export { default as Signin } from './signin';
-export { default as Signup } from './signup';
-export { default as Note } from './note';
