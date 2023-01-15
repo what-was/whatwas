02cf755dd14bf7dcf429f62565b000461498161f
Burak Saraloglu <buraksaraloglu1@gmail.com>
2023-01-15 20:53:24 +0100

    fix: remove unnecessary files (#15)

--- prisma/migrations/20230110022600_v1/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Requisition" (
-    "id" TEXT NOT NULL,
-    "userId" TEXT NOT NULL,
-    "requisitionId" TEXT NOT NULL,
-    "agreementId" TEXT NOT NULL,
-
-    CONSTRAINT "Requisition_pkey" PRIMARY KEY ("id")
-);
