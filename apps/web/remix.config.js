99da76105c1aa4565f39207876e3189358bcbe85
Burak Saraloglu <buraksaraloglu1@gmail.com>
2023-01-18 01:36:23 +0100

    initial migration v1

--- apps/web/remix.config.js
+++ apps/web/remix.config.js
@@ -1,13 +1,3 @@
-// /**
-//  * @type {import('@remix-run/dev').AppConfig}
-//  */
-// module.exports = {
-//   cacheDirectory: './node_modules/.cache/remix',
-//   ignoredRouteFiles: ['**/.*', '**/*.css', '**/*.test.{js,jsx,ts,tsx}'],
-//   serverDependenciesToBundle: ['nordigen-node', 'axios', '@clerk/backend-core'],
-//   watchPaths: ['../../packages/**/*', './app/**/*.{js,jsx,ts,tsx}'],
-// };
-
 /**
  * @type {import('@remix-run/dev').AppConfig}
  */
